d00675919f2f51025639c520af4f89c9341a8893
Supritkumar <supritk16@gmail.com>
2020-11-05 17:49:59 +0530

    rebuild arrays

--- script.js
+++ script.js
@@ -95,13 +95,36 @@ function updateDOM() {
   });
   // Run getSavedColumns only once, Update Local Storage
 
-
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
+
+// Allow arrays to reflect Drag and Drop items
+function rebuildArrays(){
+  onHoldListArray=[];
+  for (let i=0 ; i<backlogList.children.length;i++){
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray=[];
+  for (let i=0 ; i<progressList.children.length;i++){
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray=[];
+  for (let i=0 ; i<completeList.children.length;i++){
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  backlogListArray=[];
+  for (let i=0 ; i<onHoldList.children.length;i++){
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+
+  updateDOM();
+}
+
 
 // When items starts dragging
 function drag(e){
   draggedItem = e.target;
-  console.log('draggedItem->',draggedItem);
 }
 
 // When Item Enters the colum area
